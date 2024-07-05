287d7365ded53f1299417535278f07fe185a9433
rushi3691 <rushi3691@outlook.com>
2024-07-05 14:54:55 +0530

    chore: Refactor truncateTable function to support multiple tables

--- store-data-global-sections/truncate.ts
+++ store-data-global-sections/truncate.ts
@@ -1,31 +1,36 @@
 import prisma from "../db/db";
 import { createInterface } from 'readline';
 
-async function truncateTable(table: string[]) {
+async function truncateTable(tables: string[]) {
     const rl = createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
     const answer = await new Promise<string>((resolve) => {
-        console.log(`Truncating tables: \n\x1b[31m${table.join('\n')}\x1b[0m`);
+        console.log(`Truncating tables: \n\x1b[31m${tables.join('\n')}\x1b[0m`);
         rl.question('Are you sure you want to truncate these tables? (yes/no) ', (answer) => {
             resolve(answer);
         });
     });
 
     if (answer.toLowerCase() === 'yes') {
-        for (const t of table) {
-            console.log(`Truncating table: ${t}`);
-            try {
-                await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${t};`);
-                console.log(`Truncated table: ${t}`);
-            } catch (error) {
-                console.log(error)
-                console.error(`Error truncating table ${t}:`);
+        try {
+            // Disable foreign key checks
+            await prisma.$executeRawUnsafe('SET FOREIGN_KEY_CHECKS=0;');
+
+            for (const table of tables) {
+                console.log(`\x1b[31mTruncating table: ${table}\x1b[0m`);
+                await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${table};`);
+                console.log(`\x1b[32mTruncated table: ${table}\x1b[0m`);
             }
+
+            // Re-enable foreign key checks
+            await prisma.$executeRawUnsafe('SET FOREIGN_KEY_CHECKS=1;');
+            console.log('\x1b[32mTruncate operation completed.\x1b[0m');
+        } catch (error: any) {
+            console.error(`\x1b[31mError during truncate operation: ${error.message}\x1b[0m`);
         }
-        console.log('Truncate operation completed.');
     } else {
         console.log('Truncate operation cancelled.');
     }
@@ -33,6 +38,7 @@ async function truncateTable(table: string[]) {
 }
 
 // Order is important, more dependent tables should be dropped first
+// if you want to truncate Questions table, you should truncate GlobalSectionQuestions first, then GlobalSections and then Questions
 const tablesToTruncateInOrder: string[] = [
     // 'GlobalSectionQuestions',
     // 'GlobalSections',
